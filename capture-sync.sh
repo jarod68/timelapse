04f3daf5aab3c1f0d3e419a66e5bc80e3837e62c
Matthieu Holtz <matthieu.holtz@gmail.com>
2015-02-14 19:26:21 +0100

    +implement mode loop +options

--- capture-sync.sh
+++ capture-sync.sh
@@ -2,20 +2,43 @@
 
 # Configuration :
 LOCAL_PATH="./capture"
-DISTANT_PATH="//freebox/capture/"
-DELAY_S=3600
+DISTANT_PATH="//synology/capture/"
+DELAY_S=7200
 
 REAL_LOCAL_PATH=`realpath "$LOCAL_PATH"`
 REAL_DISTANT_PATH=`realpath "$DISTANT_PATH"`
 
-while [ : ]
-do
-	for f in "$REAL_LOCAL_PATH"/*
+function sync {
+
+	rsync --progress --recursive --times --remove-source-files "$REAL_LOCAL_PATH/" "$REAL_DISTANT_PATH"
+}
+
+function loop {
+
+	while [ : ]
 	do
-    		filename=`basename "$f"`
-		echo "copying $filename to $REAL_DISTANT_PATH"
-    		cp --preserve=timestamp "$REAL_LOCAL_PATH/$filename" "$REAL_DISTANT_PATH/$filename" && rm "$REAL_LOCAL_PATH/$filename"
+    		sync
+    		echo "Done! next copy in $DELAY_S sec."	
+    		sleep $DELAY_S
 	done
-    echo "Done! next copy in $DELAY_S sec."	
-    sleep $DELAY_S
+}
+
+LOOP=0
+
+while getopts ":l" opt; do
+  case $opt in
+    l)
+      echo "Loop mode is ON" ; LOOP=1 
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" 
+      ;;
+  esac
 done
+
+if [ "$LOOP" -eq 1 ]; then
+	 loop
+else 
+	sync
+	echo "Done!"
+fi
