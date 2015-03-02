16d3fedac77036d904df13b6bb57c17f9a512180
Matthieu Holtz <milou68@gmail.com>
2015-03-02 20:05:34 +0100

    + add time annotation on picture + add export of the last snap

--- capture.sh
+++ capture.sh
@@ -4,6 +4,7 @@
 SAVE_PATH="//capture"
 DELAY_S=30
 FILE_PREFIX="street"
+INSTANT_SNAPSHOT_FILENAME="//var/www/capture.jpg"
 
 #REAL_PATH=`realpath "$SAVE_PATH"`
 
@@ -13,7 +14,10 @@ do
     	mkdir -p "$DAY_SAVE_PATH"
 	REAL_PATH=`realpath "$DAY_SAVE_PATH"`
 	gpio -g write 18 1
-	raspistill -o "$REAL_PATH"/"$FILE_PREFIX"-`date +"%m-%d-%y_%Hh%M_%S"`.jpg
+	FILENAME="$REAL_PATH"/"$FILE_PREFIX"-`date +"%m-%d-%y_%Hh%M_%S"`.jpg
+	raspistill -o "$FILENAME"
     	gpio -g write 18 0
+	convert "$FILENAME" -pointsize 72 -fill red -annotate +100+100 %[exif:DateTimeOriginal] "$FILENAME"
+	cp "$FILENAME" "$INSTANT_SNAPSHOT_FILENAME"
 	sleep $DELAY_S
 done
