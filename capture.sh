f100e3efc3d383a3553d6b6190b1e811f0cba5ce
Matthieu Holtz <matthieu.holtz@gmail.com>
2015-10-11 12:29:36 +0000

    + make a version after deploying system for tests

--- capture.sh
+++ capture.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Configuration :
-SAVE_PATH="//capture"
-DELAY_S=10
+SAVE_PATH="//openwrt-nas/street/"
+DELAY_S=20
 FILE_PREFIX="street"
-INSTANT_SNAPSHOT_FILENAME="//var/www/capture.jpg"
+#INSTANT_SNAPSHOT_FILENAME="//var/www/capture.jpg"
 
 #REAL_PATH=`realpath "$SAVE_PATH"`
 
@@ -13,11 +13,11 @@ do
 	DAY_SAVE_PATH="$SAVE_PATH/"`date +"%y-%m-%d"`
     	mkdir -p "$DAY_SAVE_PATH"
 	REAL_PATH=`realpath "$DAY_SAVE_PATH"`
-	gpio -g write 18 1
+#	gpio -g write 18 1
 	FILENAME="$REAL_PATH"/"$FILE_PREFIX"-`date +"%m-%d-%y_%Hh%M_%S"`.jpg
 	raspistill -o "$FILENAME"
-    	gpio -g write 18 0
+#    	gpio -g write 18 0
 	convert "$FILENAME" -pointsize 72 -fill red -annotate +100+100 %[exif:DateTimeOriginal] "$FILENAME"
-	cp "$FILENAME" "$INSTANT_SNAPSHOT_FILENAME"
+#	cp "$FILENAME" "$INSTANT_SNAPSHOT_FILENAME"
 	sleep $DELAY_S
 done
